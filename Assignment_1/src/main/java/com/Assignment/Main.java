07ebf4030f4dca14c16ea3b7dc72ee284240cb67
Doan Ha Nguyen <doanmha96@gmail.com>
2021-12-04 22:29:36 -0600

    Add test 1. Modify Problem 1 class: testConnection now catch exception when connection to website failed. Also, test will end if exception is catch.

--- Assignment_1/src/main/java/com/Assignment/Main.java
+++ Assignment_1/src/main/java/com/Assignment/Main.java
@@ -8,10 +8,10 @@ public class Main {
         System.out.println("Hello World");
 
         WebDriver driver = initiateChromeDriver();
-        Problem_1 p1 = new Problem_1(driver);
-        p1.testConnection();
+        test_1(driver);
 
-        driver.quit();
+
+ //       driver.quit();
     }
     protected static WebDriver initiateChromeDriver(){
         String path = System.getProperty("user.dir");
@@ -20,5 +20,24 @@ public class Main {
 //        System.setProperty("webdriver.chrome.driver",path+"\\classes\\chromedriver.exe");
 
         return new ChromeDriver();
+    }
+    protected static void test_1(WebDriver d){
+        //initiate the first test
+        Problem_1 p1 = new Problem_1(d);
+        //Test if we can connect to Zwift website
+        if(p1.testConnection()){
+            System.out.println("Connection test passed");
+        }else {
+            System.out.println("Connection test failed");
+            //if we cannot connect to Zwift website, no further test can progress, so exit the program
+            System.exit(3);
+        }
+        //Test if we can find the "Accept All" button for cookies.
+        if(p1.testElementPresented()){
+            System.out.println("Element truste-consent-button is presented");
+        }else{
+            System.out.println("Element truste-consent-button is not found");
+        }
+
     }
 }
--- Assignment_1/src/main/java/com/Assignment/Problem_1.java
+++ Assignment_1/src/main/java/com/Assignment/Problem_1.java
@@ -1,6 +1,8 @@
 package com.Assignment;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Problem_1 {
     WebDriver driver;
@@ -8,8 +10,23 @@ public class Problem_1 {
         driver = d;
 
     }
-    protected void testConnection(){
-        driver.get("https://zwift.com");
-        System.out.println(driver.getTitle());
+    //Test to see if we can connect to Zwift website
+    protected boolean testConnection(){
+        //try catch block to make sure if we cannot access Zwift website, program won't crash but properly return test result.
+        try{
+            driver.get("https://zwift.com");
+            System.out.println(driver.getTitle());
+                return true;
+        }catch (Exception e){
+            System.out.print(e.getMessage());
+        }
+
+
+        return false;
+    }
+    //Test to fine the "Accept all" button when prompted for cookies.
+    protected boolean testElementPresented(){
+        WebElement testElement_2 = driver.findElement(By.id("truste-consent-button"));
+        return testElement_2.isDisplayed();
     }
 }
